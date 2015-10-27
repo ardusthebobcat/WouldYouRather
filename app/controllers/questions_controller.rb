587c986f89981e1cb64f4c98d93df7a76d4251cc
toma apostolov & michael summers <student@epicodus.com>
2015-10-26 17:00:34 -0700

    increment votes for answer A

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
   def index
-    @question = Question.all
+    @questions = Question.all
+
   end
 
   def new
@@ -22,11 +23,27 @@ class QuestionsController < ApplicationController
 
   def update
     @question = Question.find(params[:id])
-    if @question.update(question_params)
+
+  # binding.pry
+    # if params[:votes_A]
+    #     if params[:votes_A] == nil
+    #       question_vote_params[:votes_A] == 1
+    #     end
+    #     question_vote_params[:votes_A] =+1
+    #
+    #   question_vote_params[:total]  =+ 1
+      @question.update(question_params)
       redirect_to root_path
-    else
-      render :edit
-    end
+
+    # else
+    #
+    #   if @question.update(question_params)
+    #     redirect_to root_path
+    #   else
+    #     render :edit
+    #   end
+    #
+    # end
   end
 
   def destroy
@@ -37,6 +54,9 @@ class QuestionsController < ApplicationController
 
   private
   def question_params
-    params.require(:question).permit(:answer_A, :picture_A, :answer_B, :picture_B)
+    params.require(:question).permit(:answer_A, :picture_A, :answer_B, :picture_B, :votes_A, :votes_B, :total)
   end
+  # def question_vote_params
+  #   params.require(:question).permit(:votes_A, :votes_B, :total)
+  # end
 end
